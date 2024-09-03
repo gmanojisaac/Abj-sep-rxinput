d14c63d9c6b271d169a3371fa4c30a0acca647d2
gmanojisaac <gmanoj.isaac@gmail.com>
2024-09-03 12:01:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-rx-input';
+  form: FormGroup;
+  responseMessage: string = '';
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      inputData: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const inputData = this.form.value.inputData;
+      this.http.post<{ message: string }>('http://localhost:3000/api/data', { input: inputData })
+        .subscribe(
+          response => {
+            this.responseMessage = response.message;
+          },
+          error => {
+            console.error('Error occurred:', error);
+          }
+        );
+    }
+  }
 }
